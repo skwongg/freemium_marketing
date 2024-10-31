aa37643f8e16bbad7c903e97c8c28edcad39e6d1
silas <silaskwong1@gmail.com>
2024-10-31 13:31:08 -0700

    calling ollama locally without erroring

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,15 @@
+import os
+import httpx
+import ollama
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from databases import Database
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Boolean
+
+
+OLLAMA_API_URL=os.getenv("OLLAMA_API_URL", "http://localhost:11434")
 
 DATABASE_URL = "sqlite:///./test.db"
 database = Database(DATABASE_URL)
@@ -16,12 +23,15 @@ origins = [
     "https://127.0.0.1:5173",
     "http://localhost:5173",
     "https://localhost:5173",
+    "http://127.0.0.1:11434",
+    "http://localhost:11434",
     "http://localhost:*",
+    "*",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -52,7 +62,15 @@ class HomeResponse(BaseModel):
 @app.post("/search")
 async def search(query: dict):
     print("* " * 10)
-    print(query)
+    print(query.get('query'))
+    a = (call_ollama(query.get('query')))
+    print(a)
+    print(" ( )" * 10)
+    print(a)
+    
+    print("all the things happened already.")
+    
+    
     return {"results": f"Search results for: {query}"}
 
 @app.get("/hello", response_model=HelloResponse)
@@ -66,3 +84,26 @@ async def home():
 @app.get("/health")
 async def health_check():
     return {"status": "healthy"}
+
+
+@app.post("/chat")
+async def call_ollama(message: dict):
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            f"{OLLAMA_API_URL}/api/generate",
+            json={
+                "model": "llama2",
+                "prompt": message["text"]
+            }
+        )
+        print("RESPONE OF OLLAMA: ")
+        print(response.json())
+        return response.json()
+
+# def call_ollama(prompt):
+#     print("ollama is fine")
+#     resp = ollama.chat(model='mistral', messages=[{'role': 'user', 'content': 'Why is the sky blue?'}])
+#     print('ollama returned even')
+#     return resp
+    
+    
